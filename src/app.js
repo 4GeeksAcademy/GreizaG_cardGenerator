b2efae19ec8f08417516d52f0840bcaf00b67622
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-03-23 02:05:18 +0000

    [Creación archivos js, html, css] Se coloca fondo verde Se centra el número en eje X (Falta centrar en eje Y) Falta centrar correctamente el símbolo de abajo a la derecha Falta centrar toda la carta Falta cambiar tamaños de fuentes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardGenerate = () => {
+    let suit = ["♦", "♥", "♠", "♣"];
+    let cardNum = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+    let suitIndex = Math.floor(Math.random() * (suit.length - 1));
+    let cardNumIndex = Math.floor(Math.random() * (cardNum.length - 1));
+    console.log(suit[suitIndex], cardNum[cardNumIndex]);
+
+    return {
+      suit: suit[suitIndex],
+      cardNum: cardNum[cardNumIndex]
+    };
+  };
+  let { suit, cardNum } = cardGenerate();
+  console.log(suit);
+  console.log(cardNum);
+  document.querySelector("#suitUp").innerHTML = suit;
+  document.querySelector("#suitDown").innerHTML = suit;
+  document.querySelector("#cardNum").innerHTML = cardNum;
 };
